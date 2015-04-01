e53c5c06fd4b6bd24226c0638aae9a602c0189cf
MyColouringBook <shane.o-neill.4@ucdconnect.ie>
2015-04-01 17:04:50 +0100

    Tweet image start

--- src/tweetImage/image.java
+++ src/tweetImage/image.java
@@ -12,11 +12,25 @@ public class image {
 	public image(String st){
 		try {
 		    img = ImageIO.read(new File(st));
+		    int r = 255;
+		    int g = 215;
+		    int b = 0;
+		    int a = 255;
+		    int col = (a << 24) | (r << 16) | (g << 8) | b;
+		    for(int x = 452;x<818;x++){
+		    	for(int y = 24;y<518;y++){
+		    		img.setRGB(x, y, col);
+		    	}
+		    }
+		    
+		    File f = new File("resources/tweetimages/tweetFile.png");
+		    ImageIO.write(img, "PNG", f);
 		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 	
 	public static void main(String[] args){
-		image im = new image("resource/bookTemplate.png");
+		image im = new image("resources/tweetimages/bookTemplate.png");
 	}
 }
